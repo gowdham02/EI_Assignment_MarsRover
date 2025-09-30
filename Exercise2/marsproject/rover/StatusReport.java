2b98a098b0d1024100346e5b85749aa5f556f1fd
gowdham02 <gowdham2122005@gmail.com>
2025-10-01 05:26:23 +0530

    added StatusReport and integrate with main

--- Exercise2/marsproject/rover/StatusReport.java
+++ Exercise2/marsproject/rover/StatusReport.java
@@ -1,9 +1,9 @@
 package rover;
 
 public class StatusReport {
-    private int x;
-    private int y;
-    private Direction direction;
+    private final int x;
+    private final int y;
+    private final Direction direction;
 
     public StatusReport(int x, int y, Direction direction) {
         this.x = x;
@@ -13,6 +13,15 @@ public class StatusReport {
 
     @Override
     public String toString() {
-        return "Rover is at (" + x + ", " + y + ") facing " + direction;
+        // Match your previous print style
+        String facing;
+        switch (direction) {
+            case N: facing = "N"; break;
+            case E: facing = "E"; break;
+            case S: facing = "S"; break;
+            case W: facing = "W"; break;
+            default: facing = direction.toString();
+        }
+        return "Rover is at (" + x + ", " + y + ") facing " + facing;
     }
 }
